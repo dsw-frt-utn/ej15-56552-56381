a337a7d5d455af469de37cb05a354b1d3415acaf
Lopez Carlos <carlosfacundo.lopez@alu.frt.utn.edu.ar>
2026-06-19 01:08:07 -0300

    Crear dominio e interfaz de persistencia

---
+++ Dsw2026Ej15.Domain/Entities/Speciality.cs
@@ -0,0 +1,26 @@
+﻿using Dsw2026Ej15.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities;
+
+public class Speciality : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public Speciality() { }
+
+    public Speciality(string name, string description)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ValidationException("El nombre de la especialidad es requerido.");
+
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ValidationException("La descripción de la especialidad es requerida.");
+
+        Name = name;
+        Description = description;
+    }
+}
